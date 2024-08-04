541cb4629f65a05abea8678576ad7298a37a323a
SantiagoCanalesKevinEduardo <kevin.santiago.27@unsch.edu.pe>
2024-08-04 17:05:51 -0500

    Subiendo propiedades

--- src/main/java/com/pe/curso/spring/CursoSpringApplication.java
+++ src/main/java/com/pe/curso/spring/CursoSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoSpingApplication {
+public class CursoSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoSpingApplication.class, args);
+		SpringApplication.run(CursoSpringApplication.class, args);
 	}
 
 }
