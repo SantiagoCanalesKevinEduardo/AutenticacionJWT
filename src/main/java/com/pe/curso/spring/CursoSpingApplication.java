cecf6a9bc497bdeb4216d8aaabcdb87e95bf6f48
SantiagoCanalesKevinEduardo <kevin.santiago.27@unsch.edu.pe>
2024-08-04 16:33:14 -0500

    Primer commit

---
+++ src/main/java/com/pe/curso/spring/CursoSpingApplication.java
@@ -0,0 +1,13 @@
+package com.pe.curso.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/curso/spring/CursoSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.curso.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
